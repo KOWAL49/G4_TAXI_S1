2369a845691a2f04ca78c91d4d5b9c6b02d0e673
KOWAL49 <115950476+KOWAL49@users.noreply.github.com>
2023-02-17 21:43:43 +0100

    Add files via upload

---
+++ TAXI/TAXI/BAZA2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAXI
+{
+    internal class BAZA2
+    {
+        public class Dzielnica
+        {
+            public int Numer { get; set; }
+            public string Nazwa { get; set; }
+            public string Odległość_od_centrum { get; set; }
+
+
+            List<Dzielnica> Dzielnica1 = new List<Dzielnica>
+        {
+        new Dzielnica {Numer = 1, Nazwa = "Retkinia", Odległość_od_centrum = "-2 km"},
+
+        new Dzielnica {Numer = 2, Nazwa = "Łódź Kaliska", Odległość_od_centrum = "-1 km"},
+
+        new Dzielnica {Numer = 3, Nazwa = "Śródmieście", Odległość_od_centrum = "0 km"},
+
+        new Dzielnica {Numer = 4, Nazwa = "Widzew", Odległość_od_centrum = "1 km"},
+
+        new Dzielnica {Numer = 5, Nazwa = "Janów",Odległość_od_centrum = "3 km"}
+        };
+
+        }
+    }
+}
---
+++ TAXI/TAXI/baza.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    internal class baza
+    {
+        public class taksowki
+        {
+            public int id { get; set; }
+            public string samochod { get; set; }
+            public string status { get; set; }
+            public string aktualnadzielnica { get; set; }
+
+
+            List<taksowki> BAZATAXI = new List<taksowki>
+        {
+        new taksowki {id = 1, samochod = "ford mondeo", status = "wolny", aktualnadzielnica = "retkinia"},
+
+        new taksowki {id = 2, samochod = "dacia logan", status = "wolny", aktualnadzielnica = "łódź kaliska"},
+
+        new taksowki {id = 3, samochod = "toyota avensis", status = "wolny", aktualnadzielnica = "śródmieście"},
+
+        new taksowki {id = 4, samochod = "mercedes e220", status = "wolny", aktualnadzielnica = "widzew"},
+
+        new taksowki {id = 5, samochod = "huindai lantra", status = "wolny", aktualnadzielnica = "janów"}
+        };
+
+        }
+
+        public void TAXI12()
+        {
+           // foreach (int number in taksowki)
+           // {
+            //    Console.WriteLine(number);
+         //   }
+        }
+
+
+    }
+}
